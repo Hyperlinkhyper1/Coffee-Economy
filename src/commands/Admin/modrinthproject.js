8383a9ecda1cdb4468a78dac9eecf3b2084f5b11
Hyperlinkhyper <aydenjue@outlook.com>
2026-05-13 09:15:52 +0200

    channel log

--- src/commands/Admin/modrinthproject.js
+++ src/commands/Admin/modrinthproject.js
@@ -166,16 +166,6 @@ export default {
                         const latestVersion = latestVersions[0];
                         const embed = ModrinthService.createUpdateEmbed(project.projectName, project.iconUrl, latestVersion);
 
-                        // Add changelog if available
-                        if (latestVersion.body) {
-                            const changelog = latestVersion.body.length > 1024
-                                ? latestVersion.body.substring(0, 1021) + '...'
-                                : latestVersion.body;
-                            embed.addFields(
-                                { name: 'Changelog', value: changelog || 'No changelog provided', inline: false }
-                            );
-                        }
-
                         embeds.push(embed);
                     }
                 } catch (error) {
